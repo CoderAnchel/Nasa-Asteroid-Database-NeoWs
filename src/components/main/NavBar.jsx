7b73be902d3221fd347cf10dfc6669a2027ef2dd
Anchel <alex21fd@gmail.com>
2023-01-30 22:53:44 +0100

    ignition 🚀 🌏

---
+++ src/components/main/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+        <h1>THE SPACE DATABASE</h1>
+        <ul>
+            <li>Asteroids NeoWs</li>
+            <li>Mars Rover</li>
+            <li>Satellite Situation</li>
+            <li>Exoplanet</li>
+            <li>APOD</li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
